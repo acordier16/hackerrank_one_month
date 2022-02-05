015506698e12893377b43ed2b30fb60bdcb76c6f
acordier16 <acordier@scortex.io>
2022-02-05 17:06:26 +0100

    week2: add first problem statement and answer

---
+++ week2/01_sales_by_match.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    counter = dict()
+    for element in ar:
+        counter.setdefault(element, 0)
+        counter[element] += 1
+    print(counter)
+    return sum([values//2 for values in counter.values()])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
